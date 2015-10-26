6274b29ca2a65c294a2ac296f4f00093821f8b86
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-10-26 14:55:03 +0900

    add & update test

--- client_test.go
+++ client_test.go
@@ -7,12 +7,32 @@ import (
 	"testing"
 )
 
-var (
-	testClient *Client
-)
+func newTestClient() *Client {
+	return NewClient("token")
+}
 
-func init() {
-	testClient = NewClient("token")
+func TestSetName(t *testing.T) {
+	c := newTestClient()
+	if c.name != "" {
+		t.Errorf("default name is not \"\" - name: \"%s\"", c.name)
+	}
+
+	c.SetName("bot")
+	if c.name != "bot" {
+		t.Errorf("name is not \"bot\" - name: \"%s\"", c.name)
+	}
+}
+
+func TestSetAsUser(t *testing.T) {
+	c := newTestClient()
+	if c.asUser {
+		t.Errorf("default asUser is not false - asUser: %t", c.asUser)
+	}
+
+	c.SetAsUser(true)
+	if !c.asUser {
+		t.Errorf("asUser is not true - asUser: %t", c.asUser)
+	}
 }
 
 func testHandler(w http.ResponseWriter, req *http.Request) {
@@ -27,9 +47,10 @@ func TestSendMessage(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(testHandler))
 	defer server.Close()
 
-	testClient.BaseURL = server.URL
+	c := newTestClient()
+	c.BaseURL = server.URL
 
-	if err := testClient.SendMessage("#channel", "message"); err != nil {
+	if err := c.SendMessage("#channel", "message"); err != nil {
 		t.Error(err)
 	}
 }
